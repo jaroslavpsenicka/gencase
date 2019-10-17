4db6af59fcad14687b96de675588be0c5640787d
Jaroslav Psenicka <jaroslav.psenicka@gmail.com>
2019-10-17 22:22:04 +0200

    hamburger menu

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,21 +2,31 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { A } from 'hookrouter';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import './Header.css'
 
-const Header = () => (
-  <Navbar bg="white" expand="md" sticky="top">
-    <Navbar.Brand href="#home">DataCase</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link><A href="/cases">Cases</A></Nav.Link>
-        <Nav.Link><A href="/admin">Admin</A></Nav.Link>
-        <Nav.Link><A href="/about">About</A></Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+const Header = () => {
+
+  return (
+    <Navbar bg="white" expand="md" sticky="top">
+      <div className="toggle-nav" onClick={() => console.log('toggle nav')}>
+        <div className="icon-reorder tooltips" data-original-title="Toggle" data-placement="bottom">
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+      </div>
+      
+      <Navbar.Brand href="/cases">DataCase</Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <A className="nav-link" href="/cases">Cases</A>
+          <A className="nav-link" href="/admin">Admin</A>
+          <A className="nav-link" href="/about">About</A>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
 export default Header;
--- client/pages/About.js
+++ client/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useTitle } from 'hookrouter';
 
-const About = () => (
-  <div>This is simle case management application.<br />
-    <a href="mailto:@jaroslav.psenicka@gmail.com">Contact me</a>, if you like.
-  </div>
-)
+const About = () => {
+	useTitle('About');
+
+  return (
+    <div>This is simple case management application.<br />
+      <a href="mailto:@jaroslav.psenicka@gmail.com">Contact me</a>, if you like.
+    </div>
+  )
+}
 
 export default About;
