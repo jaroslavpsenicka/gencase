6263ae620f0a650f088af6c898c2ce251ac4b963
Jaroslav Psenicka <jaroslav.psenicka@gmail.com>
2019-10-23 22:32:13 +0200

    models top filtering icons

--- client/pages/ModelsPage.js
+++ client/pages/ModelsPage.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { faStar, faAngleDown } from '@fortawesome/free-solid-svg-icons'
-import { faStar as faStarOutline } from '@fortawesome/free-regular-svg-icons'
+import { faStar, faAngleDown, faAngleUp, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarOutline, faComment } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ModelsPage = () => {
 
   const data = [ 
-    { id: 1, name: 'Mortgage', description: 'Simple mortgage case' },
-    { id: 2, name: 'Loan', description: 'Customer loan as we love it' },
-    { id: 3, name: 'Test', description: 'A brand new product for the rest of us' }
+    { id: 1, name: 'Mortgage', description: 'Simple mortgage case.', revision: 3, author: 'John Doe' },
+    { id: 2, name: 'Loan', description: 'Customer loan as we love it.', revision: 1, author: 'Mary Doe' },
+    { id: 3, name: 'Test', description: 'A brand new product for the rest of us.', revision: 1, author: 'John Doe' }
   ];
 
   const [ models, setModels ] = useState(data);
@@ -21,7 +21,7 @@ const ModelsPage = () => {
 
   const toggleDetail = (model) => {
     setModels(prev => prev.map((row) => {
-      return row.id === model.id ? {...row, detail: !row.detail} : row
+      return row.id === model.id ? {...row, detailed: !row.detailed} : row
     }));
   }
 
@@ -29,29 +29,47 @@ const ModelsPage = () => {
     <div>No, there is no model.</div>
   );
 
-  const ModelRow = props => (
-    <div className="d-flex p-2 pl-3 mb-1 mr-3 bg-white text-dark">
-      <div className="flex-grow-1">
-        <h5 className="text-primary">{props.model.name}</h5>
-        <div className="text-secondary">{props.model.description}</div>
-      </div>
-      <ModelActions model={props.model}/>
+  const ModelActions = props => (
+    <div className="pt-2 float-right">
+      <FontAwesomeIcon icon={props.model.starred ? faStar : faStarOutline} size="lg" 
+        className={props.model.starred ? 'cursor-pointer text-success' : 'cursor-pointer'}
+        onClick={() => toggleStarred(props.model)}/>
+      <FontAwesomeIcon icon={props.model.detailed ? faAngleUp : faAngleDown} size="lg" 
+        className="ml-3 cursor-pointer"
+        onClick={() => toggleDetail(props.model)}/>
     </div>
   )
 
-  const ModelActions = props => (
-    <div className="p-3">
-        <FontAwesomeIcon icon={props.model.starred ? faStar : faStarOutline} size="lg" 
-          className={props.model.starred ? 'cursor-pointer text-success' : 'cursor-pointer'}
-          onClick={() => toggleStarred(props.model)}/>
-        <FontAwesomeIcon icon={faAngleDown} size="lg" className="ml-3 cursor-pointer"
-          onClick={() => toggleDetail(props.model)}/>
-    </div>
+  const ModelDetail = props => {
+    return !props.model.detailed ? null : (
+      <div className="col-md-12 pt-3 text-secondary">
+        Revision: <span className="text-black">{props.model.revision}</span><br/>
+        Created by: <span className="text-black">{props.model.author}</span><br/>
+      </div>
+    )
+  }
+
+  const ModelRow = props => (
+    <div className="font-weight-normal">
+      <div className="p-2 pl-3 mb-2 mr-3 bg-white text-dark">
+        <div className="col-md-12">
+          <ModelActions model={props.model} />
+          <h5 className="text-primary">{props.model.name}</h5>
+          <div className="text-secondary">{props.model.description}</div>
+        </div>
+        <ModelDetail model={props.model} />
+      </div>
+    </div>  
   )
 
   return (  
     <div>
-      <h5 className="text-muted font-weight-light text-uppercase mb-3">Models</h5>
+      <h4 className="text-muted font-weight-light text-uppercase mb-4 mr-3">
+        <FontAwesomeIcon icon={faPlus} className="float-right"/>
+        <FontAwesomeIcon icon={faComment} className="mr-4 float-right"/>
+        <FontAwesomeIcon icon={faStarOutline} className="mr-4 float-right"/>
+        Models
+      </h4>
       { models.length > 0 ? models.map(m => <ModelRow model={m} key={m.id} />) : <NoModels /> }
     </div>
   )
