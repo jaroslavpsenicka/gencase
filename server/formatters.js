8c5c832db4d8f82bccd7033b325930e350f589d0
Jaroslav Psenicka <jaroslav.psenicka@gmail.com>
2019-12-09 23:46:12 +0100

    formats separated

---
+++ server/formatters.js
@@ -0,0 +1,73 @@
+const Hashids = require('hashids/cjs');
+const Handlebars = require('handlebars');
+const HandlebarsDateFormat = require('handlebars-dateformat')
+
+Handlebars.registerHelper('dateFormat', HandlebarsDateFormat);
+
+const toObject = (map) => {
+	const obj = {};
+	map.forEach ((v,k) => { obj[k] = v });
+	return obj;
+}
+
+const toArray = (obj) => {
+	const arr = [];
+	Object.keys(obj).forEach(k => arr[k] = obj[k]);
+	return arr;
+}
+
+const formatCaseList = (caseObject, model) => {
+	return {
+		...caseObject._doc,
+		name: model.nameFormat ? formatValue(model.nameFormat, caseObject) : caseObject.name,
+		description: model.descriptionFormat ? formatValue(model.descriptionFormat, caseObject) : caseObject.description
+	}
+}
+
+const formatCase = (caseObject, model) => {
+	return {
+		...formatCaseList(caseObject, model),
+		detail: model.detailFormat ? formatCaseDetail(caseObject, model) : toArray(toObject(caseObject.data))
+	}
+}
+
+const formatCaseOverview = (caseObject, model) => {
+	return model.overviewFormat.map(f => {
+		return {
+			name: f.name,
+			value: f.value && f.value.indexOf('{{') > -1 ? 
+				Handlebars.compile(f.value)({...caseObject._doc, data: toObject(caseObject.data)}) : 
+				caseObject[f.value]
+		}
+	});
+}
+
+const formatProcessUrl = (caseObject, url) => {
+  return formatValue(url, caseObject);
+}
+
+const formatProcessBody = (caseObject, payload) => {
+  const body = {};
+ 	Object.keys(payload).forEach(k => body[k] = formatValue(payload[k], caseObject));
+	return body;
+}
+
+const formatCaseDetail = (caseObject, model) => {
+	return model.detailFormat.map(f => {
+		return {
+			name: f.name,
+			value: f.value && f.value.indexOf('{{') > -1 ? 
+				Handlebars.compile(f.value)({...caseObject._doc, data: toObject(caseObject.data)}) : 
+				caseObject[f.value]
+		}
+	});
+}
+
+const formatValue = (format, caseObject) => {
+	return Handlebars.compile(format)({...caseObject._doc, data: toObject(caseObject.data)});
+}
+
+module.exports = {
+  formatCaseList, formatCase, formatCaseOverview,
+  formatProcessUrl, formatProcessBody
+} 
